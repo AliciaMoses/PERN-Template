185bb37a4e8958cadc079ded50edbfce4df1dfe7
Alicia Moses <aliciamoses.dev@gmail.com>
2023-05-10 22:21:09 +0100

    Added db connection check to app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,32 +1,43 @@
-import express, { Express } from "express";
-import cors from "cors";
-import { createClient } from "@supabase/supabase-js";
-import testRouter from "./routers/testRouter";
-
-//import router from './routes/Router';
+import express, { Express } from 'express';
+import cors from 'cors';
+import { createClient } from '@supabase/supabase-js';
+import testRouter from './routers/testRouter';
 
 const createApp = (): Express => {
-  // Initialize the Express.js app
+
   const app = express();
 
-  // Middleware
   app.use(express.json());
   app.use(cors());
 
-  // Connect to Supabase
   const supabaseUrl = process.env.SUPABASE_URL;
   const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
   if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error(
-      "Missing SUPABASE_URL or SUPABASE_ANON_KEY environment variables"
+      'Missing SUPABASE_URL or SUPABASE_ANON_KEY environment variables'
     );
   }
 
   const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-  // Routes
-  app.use("/api", testRouter(supabase));
+  // Check database connection
+  supabase
+  .from('test')
+  .select()
+  .limit(1)
+  .then(({ data, error }) => {
+    if (error) {
+      console.error('Failed to connect to the Supabase database:', error);
+    } else {
+      console.log('Connected to the Supabase database successfully');
+    }
+    console.log(data)
+  });
+
+
+
+  app.use('/api', testRouter(supabase));
 
   return app;
 };
